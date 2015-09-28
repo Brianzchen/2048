0c04e35f93dd6c7032c27d4abde7f5b7d0dee0aa
Brian Chen <bchen.chen93@gmail.com>
2015-09-28 15:12:03 +1300

    feat: If user loses it now displays a pic of the group laughing

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Wow, you're bad!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
